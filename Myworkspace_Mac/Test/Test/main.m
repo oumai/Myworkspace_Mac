d48fa508d287a3ce3af790c9b4cd3f87af15d4ee
oumai <oumaiko@yahoo.com>
2018-09-26 18:07:48 +0800

    首次提交

---
+++ Myworkspace_Mac/Test/Test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test
+//
+//  Created by kmom on 2018/9/26.
+//  Copyright © 2018年 kmom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ Myworkspace_Mac/修改模板/修改模板/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  修改模板
+//
+//  Created by kmom on 2018/9/26.
+//  Copyright © 2018年 kmom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        /*
+         修改项目模板以及main函数中的内容:
+         /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates/Mac/Application/Command\ Line\ Tool.xctemplate/TemplateInfo.plist
+         
+         
+         修改OC文件头部的描述信息
+         /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File\ Templates/Source/Cocoa\ Class.xctemplate/TemplateInfo.plist
+         
+         */
+    }
+    return 0;
+}
